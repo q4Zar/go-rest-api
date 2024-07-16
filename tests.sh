79de0da1100cc3d22d731f103cf9467bdcf612b7
Damien QazarCloud <damien@qazar.cloud>
2024-07-16 11:10:40 +0200

    added some fails

--- tests.sh
+++ tests.sh
@@ -20,18 +20,25 @@ echo $currency_dollar
 # 3
 echo 'creates assets'
 
+# EUR
 asset_euro_success=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 1, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
 echo $asset_euro_success
-
+# wrong fk currency
 asset_euro_fails_1=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 3, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
 echo $asset_euro_fails_1
+# duplicate on index currency & user
+asset_euro_fails_2=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 1, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
+echo $asset_euro_fails_2
 
+#USD
 asset_dollar_success=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 2, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
 echo $asset_dollar_success
-
+# wrong fk currency
 asset_dollar_fails_1=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 3, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
 echo $asset_dollar_fails_1
-
+# duplicate on index currency & user
+asset_dollar_fails_1=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currencyID": 2, "userID": 1, "amount" : 12356.54897}' -H "Content-Type: application/json" http://127.0.0.1:8080/assets)
+echo $asset_dollar_fails_1
 # balance_dollar=$(curl -X POST -H "Authorization: Bearer $token" -d '{"currency": "USD", "amount" : 1000}' -H "Content-Type: application/json" http://127.0.0.1:8080/balances)
 # echo $currency_dollar
 
